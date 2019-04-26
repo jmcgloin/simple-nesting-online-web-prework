baae7471b4a3494d79fd1f42457ce3dedccdadff
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-26 18:42:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -116,6 +116,7 @@ def changing_alan
      
     # programmer_hash[:alan_kay][:known_for] = alans_new_info
     puts programmer_hash[:alan_kay][:known_for]
+    puts alans_new_info
      
      
 end
